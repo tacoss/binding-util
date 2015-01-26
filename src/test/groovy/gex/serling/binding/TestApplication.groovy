2c446eeaa1a23e0499bbfb2dfeece207d369ca90
Anallely Olivares Toledo <anaoko@gmail.com>
2015-01-26 17:29:03 -0600

    Tests now works, but i am not happy with grails version

---
+++ src/test/groovy/gex/serling/binding/TestApplication.groovy
@@ -0,0 +1,17 @@
+package gex.serling.binding
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+
+/**
+ * Created by Tsunllly on 1/26/15.
+ */
+@SpringBootApplication
+class TestApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run TestApplication, args
+  }
+
+}
--- src/test/groovy/gex/serling/binding/UtilSpec.groovy
+++ src/test/groovy/gex/serling/binding/UtilSpec.groovy
@@ -1,11 +1,21 @@
 package gex.serling.binding
 
+import spock.lang.IgnoreRest
 import spock.lang.Specification
+import org.springframework.boot.test.IntegrationTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * Created by domix on 12/26/14.
  */
+@IntegrationTest
+@ContextConfiguration(loader = SpringApplicationContextLoader, classes = TestApplication)
+@Transactional
 class UtilSpec extends Specification {
+
+
   def 'should bind a new Instance taking a instanciated object'() {
     when:
       def util = new Util()
@@ -13,6 +23,26 @@ class UtilSpec extends Specification {
     then:
       object.name == 'name'
   }
+
+  @IgnoreRest
+  def 'test'(){
+    given:
+      ExampleDomain domain = new ExampleDomain(name: 'Cloud')
+      println domain
+      domain.save(flush: true)
+      println domain.properties
+
+    when:
+      ExampleDto dto = Util.bind(domain, ExampleDto.class)
+      println dto.properties
+
+    then:
+      dto.name == domain.name
+  }
+
+
+
+
 }
 
 class Demo {
